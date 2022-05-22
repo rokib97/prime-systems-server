bf39d8719a9403fe1ac17cc82196a51673035a0c
rokib97 <hasanrokib1997@gmail.com>
2022-05-22 19:47:32 +0600

    initial setup for the server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// root
+app.get("/", async (req, res) => {
+  res.send("Hello from Prime Systems");
+});
+
+// port
+app.listen(port, () => {
+  console.log(`Prime Systemsserver is running from port ${port} `);
+});
