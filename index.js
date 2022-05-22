b3aee371b83c2c7c63872fd1df54b4032a31b510
rokib97 <hasanrokib1997@gmail.com>
2022-05-22 23:33:08 +0600

    added api to fetch single iteam data using id params

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,38 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvdtz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+
+    const partsCollection = client.db("PrimeSystems").collection("parts");
+
+    // get all parts items api
+    app.get("/get-parts", async (req, res) => {
+      const parts = await partsCollection.find({}).toArray();
+      res.send(parts);
+    });
+
+    // get single items api
+    app.get("/get-parts/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await partsCollection.findOne(query);
+      res.send(result);
+    });
+
+    console.log("db connected");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 // root
 app.get("/", async (req, res) => {
   res.send("Hello from Prime Systems");
