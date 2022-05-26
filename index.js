111d97fb060ebc96501cd1356681e0bca048780d
rokib97 <hasanrokib1997@gmail.com>
2022-05-26 20:30:25 +0600

    added verify admin api

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware
+//middlewares
 app.use(cors());
 app.use(express.json());
 
