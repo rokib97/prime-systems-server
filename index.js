d58fb114613817061dea6dde086dccac0558e681
rokib97 <hasanrokib1997@gmail.com>
2022-05-26 21:58:10 +0600

    added verify admin api

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// connecting to database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvdtz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
